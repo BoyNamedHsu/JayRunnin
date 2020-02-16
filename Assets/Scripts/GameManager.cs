6f191e208c81e53263f3c85f75982436d6e79f69
BoyNamedHsu <44332341+BoyNamedHsu@users.noreply.github.com>
2020-02-16 03:12:02 -0800

    test

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    enum Direction {North, East, South, West, None};
+    private Overworld grid;
+    public static (int x, int y) playerStart = (0, 0);
+    public static List<(int x, int y)> directions;
+    public static List<Living> followers;
+    public static CarTile car;
+    // Add enum for object and elements
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        directions = new List<(int x, int y)>();
+        followers = new List<Living> { 
+            new Jay(playerStart.x, playerStart.y), 
+            new Follower(1, 0, false), 
+            new Follower(2, 0, false), 
+            new Follower(3, 0, false) };
+        grid = GameObject.Find("Overworld").GetComponent<Overworld>();
+        for (int i = followers.Count - 2; i >= 0; i--) {
+            directions.Add(followers[i].position);
+        }
+        car = new CarTile(3, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Direction moved = Move();
+        if (moved != Direction.None)
+        {
+            // Entering this branch assumes that move is valid.
+
+            Tile[,] currentGrid = grid.getGrid();
+            
+            // Test if valid move
+            for (int i = 1; i < followers.Count; i++)
+            {
+                /* (int x, int y) next = followers[i].Move(directions[directions.Count - i - 1]);*/
+                // don't need to subtract index by 1 because start i at 1
+                grid.Move(followers[i].position, directions[directions.Count - i], 'f');
+                followers[i].position = directions[directions.Count - i];
+                print(followers[i].position);
+
+            }
+            directions.Add(followers[0].position);
+            directions.RemoveAt(0);
+
+            car.countDown();
+            Debug.Log(car.countdown);
+            if (car.gone && car.countdown == 1) {
+                grid.kill(car.yPos);
+            }
+
+        }
+        //print(string.Join(",", directions));
+    }
+
+    // Returns the direction Jay has moved, else returns null if an invalid move occurs
+    Direction Move()
+    {
+        Direction dir = Direction.None;
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        (int x, int y) temp = followers[0].position;  // Jay's position
+        if (Input.GetButtonDown("Vertical"))
+        {
+            if (moveVertical < 0 && temp.x < grid.height - 1)  // South
+            {
+                temp = (temp.x + 1, temp.y);
+                if (grid.TileOccupied(temp)) dir = Direction.South;
+            }
+            else if (moveVertical > 0 && temp.x > 0)  // North
+            {
+                temp = (temp.x - 1, temp.y);
+                if (grid.TileOccupied(temp)) dir = Direction.North;
+            }
+        }
+        else if (Input.GetButtonDown("Horizontal"))
+        {
+            if (moveHorizontal > 0 && temp.y < grid.width - 1)  // East
+            {
+                temp = (temp.x, temp.y + 1);
+                if (grid.TileOccupied(temp)) dir = Direction.East;
+            }
+            else if (moveHorizontal < 0 && temp.y > 0)  // West
+            {
+                temp = (temp.x, temp.y - 1);
+                if (grid.TileOccupied(temp)) dir = Direction.West;
+            }
+        }
+
+        if (grid.Move(followers[0].position, temp, 'c'))
+        {
+            followers[0].position = temp;
+        }
+        print(string.Join(",", directions));
+        print(dir);
+        return dir;
+    }
+
+
+}
---
+++ Assets/Scripts/Overworld.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Overworld : MonoBehaviour
+{
+    private Tile[,] gridworld;
+    public int height;
+    public int width;
+    public TextMeshProUGUI ian;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gridworld = new Tile[height, width];
+        for (int i = 0; i < height; i++)
+            for (int j = 0; j < width; j++)
+                gridworld[i, j] = new Tile();
+
+        gridworld[GameManager.playerStart.x, GameManager.playerStart.y].character = 'c';
+        for (int i = 1; i < GameManager.followers.Count(); i++)
+        {
+            gridworld[GameManager.followers[i].position.x, 
+                        GameManager.followers[i].position.y].character = 'f';
+        }
+        ian.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ian.text = toString();
+    }
+
+    public string toString()
+    {
+        string tess = "";
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                tess += gridworld[i, j].character;
+            }
+            tess += Environment.NewLine;
+        }
+        return tess;
+    }
+
+    public bool Move((int x, int y) prev, (int x, int y) current, char character)
+    {
+        if (TileOccupied(current))
+        {
+            gridworld[prev.x, prev.y].character = '0';
+            gridworld[current.x, current.y].character = character;
+            return true;
+        }
+        return false;
+    }
+
+    public void kill(int col) {
+        for (int x = 0; x < height; x++) {
+            if (gridworld[x, col].character != '0') {
+                gridworld[x, col].character = '0';
+                for (int i = 0; i < GameManager.followers.Count; i++)
+                {
+                    if (GameManager.followers[i].position == (x, col)) {
+                        GameManager.followers.RemoveAt(i);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public Tile[,] getGrid() {
+        return gridworld;
+    }
+
+    // Returns whether or not coords is occupied
+    public bool TileOccupied((int x, int y) coords)
+    {
+        return gridworld[coords.x, coords.y].character == '0';
+    }
+}
