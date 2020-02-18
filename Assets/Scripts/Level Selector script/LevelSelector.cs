9251f84fda63baedc3e37255af44e4698e00aaca
Kelvin Chung <kelvincc@cs.washington.edu>
2020-02-18 10:03:22 -0800

    added Main Menu, Settings, and Overworld scenes

---
+++ Assets/Scripts/Level Selector script/LevelSelector.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelector : MonoBehaviour
+{
+    public LevelChanger fader;
+
+    public void Select(int levelIndex)
+    {
+        fader.FadeToLevel(levelIndex);
+    }
+}
