5682b0d33928c5c8a27a45158fc3090acfae5f43
Ian Wohlers <ianw9@cs.washington.edu>
2020-03-04 20:36:12 -0800

    Add opacity to non-unlocked levels

---
+++ Assets/Scripts/HideIfLocked.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HideIfLocked : MonoBehaviour
+{
+    public int levelNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Image>().color = (levelNum <= Unlocker.GetHighestUnlockedLevel()) ?
+            new Color32(238,238,238,255) : new Color32(238,238,238,50);
+    }
+}
